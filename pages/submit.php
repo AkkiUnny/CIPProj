fe7273ee8c7af85e853a4f8bd3b827ee95fde771
polyeit <poalabarroso@gmail.com>
2026-06-04 21:40:50 +0800

    further upload and homepage list update

--- pages/submit.php
+++ pages/submit.php
@@ -6,25 +6,27 @@
                     $destFolder = $targetDir . basename($_FILES['docFile']['name']);
                     $tempfile = $_FILES['docFile']['tmp_name'];
 
-                    echo $destFolder . "<br>";
-                    echo $tempfile . "<br>";
+                    // echo $destFolder . "<br>";
+                    // echo $tempfile . "<br>";
                 
                     if(move_uploaded_file($tempfile, $destFolder)){
-                        echo ("File is daijoubu");
+                        echo ("Your paper has been uploaded successfully!");
                     } else {
-                        echo ("File is NOT daijoubu");
+                        echo ("Trying to upload your paper again. Make sure the file is not too large and is in an accepted format.");
                     }
 
                     $targetDir = "FileFolder/";
                     $dirlist = scandir($targetDir, 1); 
 
-                    foreach($dirlist as $listing){
-                        echo "<a href='FileFolder/" . urlencode($listing) . "'> {$listing} </a><br> ";
-                    }
+                    // foreach($dirlist as $listing){
+                    //     echo "<a href='FileFolder/" . urlencode($listing) . "'> {$listing} </a><br> ";
+                    // }
 
                 }
 
+    if (isset($_POST['Upload'])) {
     uploadfile();
+    }
 ?>
 <div class="panel">
     <div class="panel-title">Submit Research Paper</div>
@@ -33,6 +35,6 @@
         <br>
         <input type="file" name="docFile">
         <br>
-        <input type="submit" value="Upload">
+        <input type="submit" name="Upload" value="Upload">
     </form>
 </div>
