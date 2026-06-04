93b6bde6eec90d57cfcad03370eee063d19b14f1
polyeit <poalabarroso@gmail.com>
2026-06-04 22:20:52 +0800

    nuked the banana

--- pages/home.php
+++ pages/home.php
@@ -27,5 +27,5 @@ if (is_dir($targetDir)) {
         <p>No files found in FileFolder.</p>
     <?php endif; ?>
 
-    <div class="tenor-gif-embed" data-postid="8741534806163341101" data-share-method="host" data-aspect-ratio="1.55245" data-width="100%"><a href="https://tenor.com/view/spinning-banana-banana-donkey-kong-gif-8741534806163341101">Spinning Banana Donkey Kong Sticker</a>from <a href="https://tenor.com/search/spinning+banana-stickers">Spinning Banana Stickers</a></div> <script type="text/javascript" async src="https://tenor.com/embed.js"></script>
+    <!-- <div class="tenor-gif-embed" data-postid="8741534806163341101" data-share-method="host" data-aspect-ratio="1.55245" data-width="100%"><a href="https://tenor.com/view/spinning-banana-banana-donkey-kong-gif-8741534806163341101">Spinning Banana Donkey Kong Sticker</a>from <a href="https://tenor.com/search/spinning+banana-stickers">Spinning Banana Stickers</a></div> <script type="text/javascript" async src="https://tenor.com/embed.js"></script> -->
 </div>
